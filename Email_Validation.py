b0b7c2e98de0abca462d1c2e16e27515aefbae23
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-07 04:47:20 -0700

    Full form or re

--- Email_Validation.py
+++ Email_Validation.py
@@ -5,8 +5,8 @@
 4. @ use in last 3 or 2 postion
 
 """
-
-import re
+# re mean Regular Expression
+import re 
 
 Email_Condition = "^[a-z]+[0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
 user_email = input("Enter your email ")
