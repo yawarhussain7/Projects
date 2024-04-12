01d6a25828440ee92ced982bcfc0f1eadf20bfd9
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-12 04:52:19 -0700

    Qrcode Progrma

---
+++ Qrcode/QR_code_01.py
@@ -0,0 +1,18 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4  # Fixed typo here
+)
+
+qr.add_data("https://chat.openai.com/")
+
+qr.make(fit=True)
+
+image = qr.make_image(fill_color="pink", back_color="red")  # Adjusted color names to lowercase
+
+image.save("Color.png")
